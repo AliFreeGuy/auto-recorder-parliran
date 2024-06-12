27e93798b59024256c0dd81802594cd4f2f12e4e
AliFreeGuy <alifreeguy@gmail.com>
2024-06-12 12:32:01 +0330

    feat : show recorders in checker task

--- app/tasks/recorder_tasks.py
+++ app/tasks/recorder_tasks.py
@@ -2,9 +2,24 @@
 
 from celery import Celery
 from celery.schedules import crontab
+import redis
+from os import environ as env
+from os.path import abspath, dirname
+import sys
 
+
+parent_dir = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, parent_dir)
+
+from utils import cache
+
+
+REDIS_HOST = env.get('REDIS_HOST')
+REDIS_PORT = env.get('REDIS_PORT')
+REDIS_DB= env.get('REDIS_DB')
+
+r = redis.Redis(host=REDIS_HOST , port=REDIS_PORT , db=REDIS_DB , decode_responses=True)
 app = Celery('tasks', broker='redis://localhost:6379/0')
-
 app.conf.timezone = 'UTC'
 app.conf.update(
     task_serializer='json',
@@ -25,17 +40,21 @@ app.conf.beat_schedule = {
 
 
 
+
 @app.task
 def checker():
-    print("سلام")
-    recorder.delay()
+    recorders = cache.recorders()
+    for rec in recorders :
+        print(rec['start_time'])
+
+    recorder_task.delay()
 
 
 
 
 
-@app.task(name = 'tasks.recorder' , bind = True , default_retry_delay=1)
-def recorder(self):
+@app.task(name = 'tasks.recorder_task' , bind = True , default_retry_delay=1)
+def recorder_task(self):
     print('thi is recorder')
 
 
